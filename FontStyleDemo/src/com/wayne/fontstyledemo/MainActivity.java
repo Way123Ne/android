0ad9f56d9d08d6e130dc644c046f5afe286682b7
Way123Ne <1657886486@qq.com>
2018-11-18 16:34:14 +0800

    Add files via upload

---
+++ FontStyleDemo/src/com/wayne/fontstyledemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.wayne.fontstyledemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+private Button btn_changecolor;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		
+	}
+	
+	
+}
